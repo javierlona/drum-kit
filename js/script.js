0858627b6e74bd9861c1f16218fc9f654037fbd3
Javier Lona <javierlona1@gmail.com>
2019-03-12 08:49:44 -0500

    Created new function play_sound

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-window.addEventListener('keydown', function(e) {
+const keys = document.querySelectorAll(".key");
+// console.log(keys);
+
+function play_sound(e) {
   // console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -7,17 +10,12 @@ window.addEventListener('keydown', function(e) {
   audio.currentTime = 0; // Rewind to the start
   audio.play();
   key.classList.add('playing');
-
-
-}, false);
-
-const keys = document.querySelectorAll(".key");
-console.log(keys);
+}
 
 function remove_transition(e) {
   if(e.propertyName !== 'transform') return;
   // console.log(e);
-  console.log(this);
+  // console.log(this);
   this.classList.remove("playing");
 }
 
@@ -25,6 +23,8 @@ function remove_transition(e) {
 // for(i=0; i < keys.length; i++) {
 //   keys[i].addEventListener("transitionend", remove_transition, false)
 // }
-
+  
 // New ES6 method
 keys.forEach(key => key.addEventListener("transitionend", remove_transition, false));
+
+window.addEventListener('keydown', play_sound, false);
