2bcb4f76bc6af33d35775c97e6b5015ac7d5257d
Javier Lona <javierlona1@gmail.com>
2019-03-12 08:40:50 -0500

    Adding foreach function to tie keys to eventlistener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(key);
+  // console.log(key);
   if(!audio) return;
   audio.currentTime = 0; // Rewind to the start
   audio.play();
@@ -10,3 +10,21 @@ window.addEventListener('keydown', function(e) {
 
 
 }, false);
+
+const keys = document.querySelectorAll(".key");
+console.log(keys);
+
+function remove_transition(e) {
+  if(e.propertyName !== 'transform') return;
+  // console.log(e);
+  console.log(this);
+  this.classList.remove("playing");
+}
+
+// Old method
+// for(i=0; i < keys.length; i++) {
+//   keys[i].addEventListener("transitionend", remove_transition, false)
+// }
+
+// New ES6 method
+keys.forEach(key => key.addEventListener("transitionend", remove_transition, false));
