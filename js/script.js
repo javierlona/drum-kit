d55198462464b27270a60e38ed3b46c604ae1af9
Javier Lona <javierlona1@gmail.com>
2019-03-11 20:34:23 -0500

    Add class playing when hitting key

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function(e) {
+  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(key);
+  if(!audio) return;
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
+  key.classList.add('playing');
+
+
+}, false);
